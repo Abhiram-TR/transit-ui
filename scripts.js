c72c1a5fa0da082fab75ee05b698a1926cd444b1
Abhiram T <117012619+Abhiram-TR@users.noreply.github.com>
2024-03-06 19:15:39 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const staffButton = document.getElementById("button2");
+
+    staffButton.addEventListener("click", function() {
+        window.location.href = "staff.html"; // Redirect to the staff page
+    });
+});

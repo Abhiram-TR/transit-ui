7982f0e84322e9c0dd0b061616fab895b9e5aa7d
Abhiram T <117012619+Abhiram-TR@users.noreply.github.com>
2024-03-18 02:52:23 +0530

    Update staff_script.js
    added login feature

--- staff_script.js
+++ staff_script.js
@@ -1,28 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Staff Page</title>
-    <link rel="stylesheet" href="staff_styles.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet"> <!-- Import Montserrat font -->
-</head>
-<body>
-    <div class="left-half">
-        <div class="transit-text">Transit Tracker</div>
-    </div>
-    <div class="right-half">
-        <div class="login-box">
-            <label for="username">Log in</label>
-            <input type="email" id="email" placeholder="Email">
-            <input type="password" id="password" placeholder="Password">
-            <button id="login_button">Login</button>
-            <button id="signup_button" class="signup-btn">Sign Up</button> <!-- Sign Up button -->
-        </div>
-    </div>
-   <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"></script>
-    <script  src="staff_script.js" type="module"> // Your JavaScript code goes here</script>
-</body>
-</html>
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getDatabase, ref, push, update } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+const appsettings = {
+    databaseURL: "https://sample-7ef53-default-rtdb.firebaseio.com/"
+};
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwX2pvwgfJX8Ho0j5wKVVxaayyKADeFdE",
+  authDomain: "sample-7ef53.firebaseapp.com",
+  databaseURL: "https://sample-7ef53-default-rtdb.firebaseio.com",
+  projectId: "sample-7ef53",
+  storageBucket: "sample-7ef53.appspot.com",
+  messagingSenderId: "197922425298",
+  appId: "1:197922425298:web:d94499ab1c0462cc99e00b"
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    const signupButton = document.getElementById("signup_button");
+
+    signupButton.addEventListener("click", (e) => {
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+
+       createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                alert('ACCOUNT CREATED');
+              
+
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(errorMessage);
+            });
+
+        });
+});
+
+
+
+
+ document.addEventListener("DOMContentLoaded", (e) => {
+    const loginButton = document.getElementById("login_button");
+
+    loginButton.addEventListener("click", (e) => {
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+
+           signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+               // Signed in 
+             const user = userCredential.user;
+            // alert('Logined');
+             window.location.href = "staff_profile.html";
+                    // ...
+             })
+               .catch((error) => {
+               const errorCode = error.code;
+                const errorMessage = error.message;
+
+                alert(errorMessage);
+             });
+
+});
+});

5480b686b74bf02f99e8019b46d965f979af743f
Abhiram T <117012619+Abhiram-TR@users.noreply.github.com>
2024-03-18 02:51:45 +0530

    Update staff_script.js
    added login feature

--- staff_script.js
+++ staff_script.js
@@ -1,19 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const loginButton = document.getElementById("login_button");
-
-    loginButton.addEventListener("click", function() {
-        // Redirect to staff profile page
-        window.location.href = "staff_profile.html";
-
-        // Ask for location permission
-      /*  if (navigator.permissions) {
-            navigator.permissions.query({name:'geolocation'}).then(function(permissionStatus) {
-                if (permissionStatus.state === 'prompt') {
-                    alert("Please allow location access to enable location services.");
-                }
-            });
-        } else {
-            alert("Your browser does not support location permission requests.");
-        }*/
-    });
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff Page</title>
+    <link rel="stylesheet" href="staff_styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet"> <!-- Import Montserrat font -->
+</head>
+<body>
+    <div class="left-half">
+        <div class="transit-text">Transit Tracker</div>
+    </div>
+    <div class="right-half">
+        <div class="login-box">
+            <label for="username">Log in</label>
+            <input type="email" id="email" placeholder="Email">
+            <input type="password" id="password" placeholder="Password">
+            <button id="login_button">Login</button>
+            <button id="signup_button" class="signup-btn">Sign Up</button> <!-- Sign Up button -->
+        </div>
+    </div>
+   <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"></script>
+    <script  src="staff_script.js" type="module"> // Your JavaScript code goes here</script>
+</body>
+</html>

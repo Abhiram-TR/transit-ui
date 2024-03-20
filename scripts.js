199026e8ac92bd76636fb85fcdaf7da3606e57b5
Abhiram T <117012619+Abhiram-TR@users.noreply.github.com>
2024-03-21 01:43:07 +0530

    Update scripts.js
    added user page

--- scripts.js
+++ scripts.js
@@ -5,3 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "staff.html"; // Redirect to the staff page
     });
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const staffButton = document.getElementById("button1");
+
+    staffButton.addEventListener("click", function() {
+        window.location.href = "user.html"; // Redirect to the staff page
+    });
+});
